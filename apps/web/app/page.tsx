97c3fe4253db991d3375e03a5a45e136a910a9d8
lakshayjuneja <lakshayjuneja003@gmail.com>
2025-09-24 20:11:32 +0530

    testing ci pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst()
   return(
     <div>
-      {user?.username || "no user found: haha"}
+      {user?.username || "no user found: hoha "}
       {user?.password || "no password found"}
     </div>
   )
